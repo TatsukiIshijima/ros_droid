db68edd6952169aa1cb781e1b9f5896389f98385
TatsukiIshijima <tatsuki445@gmail.com>
2022-10-16 16:43:58 +0900

    add infrared receive process

--- catkin_ws/src/raspberrypi/script/controller.py
+++ catkin_ws/src/raspberrypi/script/controller.py
@@ -19,6 +19,10 @@ from std_msgs.msg import Int8
 from std_msgs.msg import UInt8
 
 class Controller:
+
+    __RIGHT_STEER_VALUE = 40
+    __CENTER_STEER_VALUE = 108
+    __LEFT_STEER_VALUE = 74
     
     def __init__(self):
         self.__led_pub = rospy.Publisher(ARDUINO_LED_TOPIC, Bool, queue_size=10)
@@ -30,14 +34,26 @@ class Controller:
     def __subscribe_detection(self):
         rospy.Subscriber(DETECTOR_RESULT_TOPIC, Detection2DArray, self.__process_detection_result)
 
-    def __process_detection_result(self):
+    def __process_detection_result(self, data):
         pass
 
     def __subscribe_infrared(self):
         rospy.Subscriber(ARDUINO_INFRARED_TOPIC, Float32MultiArray, self.__process_infrared)
 
-    def __process_infrared(self):
-        pass
+    def __process_infrared(self, value_array):
+        if (len(value_array.data) != 3):
+            return
+        if (all(data < 0.1 and data >= 0.8) for data in value_array.data):
+            return
+        open_direction_index = value_array.data.index(max(value_array.data))
+        # 0: right , 1: center , 2: left
+        # rospy.loginfo('open direction index=%d', open_direction_index)
+        if open_direction_index == 0:
+            self.publish_servo(self.__RIGHT_STEER_VALUE)
+        elif open_direction_index == 2:
+            self.publish_servo(self.__LEFT_STEER_VALUE)
+        else:
+            self.publish_servo(self.__CENTER_STEER_VALUE)
 
     def subsciribe(self):
         self.__subscribe_infrared()
